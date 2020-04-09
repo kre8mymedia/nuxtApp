5e1c628eac60a47eb4a8f4501a78702f20fc6e40
unknown <kre8mymedia@gmail.com>
2020-04-09 02:32:26 -0500

    fixed logo sizing issues on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <div class="container pt-5 text-center">
       <h1>Welcome to Thesis</h1>
       <small><i>An Adaptive Technologies Company</i></small>
-      <p>Welcome to my sample NuxtJS App, with Bootstrap CDN</p>
+      <p>Welcome to my sample NuxtJS App<br>with Bootstrap CDN</p>
       <img class="mt-5" height="300px" src="https://siasky.net/vAEumK81Jb3xnTLv8HuyJMf3i0sjNof_1x5sdh0WjOnOJw" alt="Adaptive Tech Logo">
     </div>
   </div>
