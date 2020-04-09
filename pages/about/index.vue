a61d91ec46064b0ff29a3a238038f4e0686f5c38
unknown <kre8mymedia@gmail.com>
2020-04-09 02:36:01 -0500

    added fade in

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -18,3 +18,19 @@ export default {
   }
 }
 </script>
+
+<style>
+  h1, small, p, img {
+    animation: fadeInAnimation ease 3s;
+    animation-iteration-count: 1; 
+    animation-fill-mode: forwards;
+  }
+  @keyframes fadeInAnimation { 
+    0% { 
+      opacity: 0; 
+    } 
+    100% { 
+      opacity: 1; 
+     } 
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -31,4 +31,17 @@ export default {
     color: white;
     background: linear-gradient(#201866, #000000);
   }
+  h1, small, p, img {
+    animation: fadeInAnimation ease 3s;
+    animation-iteration-count: 1; 
+    animation-fill-mode: forwards;
+  }
+  @keyframes fadeInAnimation { 
+    0% { 
+      opacity: 0; 
+    } 
+    100% { 
+      opacity: 1; 
+     } 
+  }
 </style>
