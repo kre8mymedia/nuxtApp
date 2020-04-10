4d612f86c0147f0de50d75a6460abbebcd8b633b
unknown <kre8mymedia@gmail.com>
2020-04-09 22:41:48 -0500

    sudo login component made

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,29 +2,32 @@
   <div class="px-0 container-fluid">
     <Navbar></Navbar>
     <div class="row justify-content-center">
-      <div class="col-sm-6">
+      <div class="col-sm-5">
         <div class="text-center">
           <img height="300px" src="~/assets/black-logo.png" alt="Adaptive Tech Logo">
           <h1>Login Page</h1>
           <p>Please sign in below</p>
         </div>
-        <div class="pt-1 pb-2 container">
-          <form>
+        <div v-if="isLoggedin == false" class="pt-1 pb-2 container">
+          
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
               <label for="exampleInputPassword1">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
+              <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="form-group form-check">
               <input type="checkbox" class="form-check-input" id="exampleCheck1">
               <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-            <button type="button" class="btn btn-primary">Submit</button>
-          </form>
+            <button v-on:click="login()" type="button" class="btn btn-primary">Submit</button>
+          
+        </div>
+        <div v-else class="pt-1 pb-2 text-center container">
+          <h1>You have been logged in!</h1>
         </div>
       </div>
     </div>
@@ -38,6 +41,27 @@ export default {
   components: {
     Navbar,
   },
+  data() {
+    return{
+      email: "",
+      password: "",
+      isLoggedin: false,
+    }
+  },
+  methods: {
+    login() {
+      if (this.email != "" && this.password != "") {
+        if (this.email == 'ryan.eggleston@tmgcore.com' && this.password == 'nixx1234') {
+          this.isLoggedin = true
+          console.log(this.email, this.password)
+        } else {
+          console.log('You have entered an invalid login!')
+        }
+      } else {
+        console.log("please enter email and password")
+      }
+    }
+  }
 }
 </script>
 
