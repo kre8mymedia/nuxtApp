b57739837374c976422291904f942bba170b58dd
unknown <kre8mymedia@gmail.com>
2020-04-09 23:18:55 -0500

    added retreive password page

--- pages/login/retrieve.vue
+++ pages/login/retrieve.vue
@@ -3,31 +3,24 @@
     <Navbar :isLoggedin="isLoggedin"></Navbar>
     <div class="row justify-content-center">
       <div class="col-sm-4">
+
         <div class="text-center">
           <img height="300px" src="~/assets/black-logo.png" alt="Adaptive Tech Logo">
-          <h1>Login Page</h1>
-          <p>Please sign in below</p>
+          <h2>Retrieve Password</h2>
+          <p>Please enter an email address<br>to send new password</p>
         </div>
+
+        <!-- Retreival Email -->
         <div v-if="isLoggedin == false" class="pt-1 pb-1 container">
-          
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
               <small class="form-text text-muted"><font id="emailHelp" color="red"></font></small>
             </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="form-group">
-              <a href="/login/retrieve">Forgot password?</a>
-            </div>
             <button v-on:click="login()" type="button" class="btn btn-primary">Submit</button>
-          
         </div>
-        <div v-else class="pt-1 pb-2 text-center container">
-          <h1>You have been logged in!</h1>
-        </div>
+        <!-- END Retreival Email -->
+
       </div>
     </div>
   </div>
